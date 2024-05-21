26051a0b9b4fbc605f72d2903ceb7ac9d832ecda
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-21 16:54:12 +0530

    "Simplified scientificNotation class by removing unnecessary variable and combining conditional return statement."

--- src/scientificNotation.js
+++ src/scientificNotation.js
@@ -1,11 +1,8 @@
 class scientificNotation {
   convertToScientificNotation(input) {
     const value = parseFloat(input);
-    if (typeof value != "number") {
-      return "Invalid input";
-    }
-    const scientificNotation = value.toExponential(2);
-    return scientificNotation;
+    if (typeof value != "number") return "Invalid input";
+    return value.toExponential(2);
   }
 }
 
