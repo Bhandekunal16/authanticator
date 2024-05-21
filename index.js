66c148b451d181a61db35cbe5b1925717ed990be
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-21 14:15:34 +0530

    "Updated application function to call hexToString with '6b756e616c' instead of convertToBinary"

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Scientific_Notation = require("./main");
 
 function application() {
-  return Scientific_Notation.binary.convertToBinary();
+  return Scientific_Notation.hexadecimal.hexToString('6b756e616c');
 }
 
 console.log(application());
