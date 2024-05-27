a9d7cc6b7393fd4c27c8e52c3b5e86b5e1af4165
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-27 12:27:45 +0530

    Added new method `octalArray` to `OctalDecimal` class, which converts an array of strings to an array of octal strings.

---
+++ src/algorithum.js
@@ -0,0 +1,42 @@
+const [Hexadecimal, OctalDecimal, scientificNotation, Binary] = [
+  require("./hexadecimal"),
+  require("./octalDecimal"),
+  require("./scientificNotation"),
+  require("./binary"),
+];
+
+class Algorithm {
+  encrypt(publicKey, data) {
+    try {
+      const privateKey = new Binary().convertToBinary("Robotic");
+      const tail = new Hexadecimal().hexadecimalConvert(publicKey);
+      const head = new scientificNotation().convertToScientificNotation(
+        parseInt(privateKey)
+      );
+      const body =
+        data.length > 0 && typeof data == "object"
+          ? new OctalDecimal().octalArray(data)
+          : new OctalDecimal().octal(data);
+      return `${head}${body}${tail}`;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  decrypt(publicKey, data) {
+    const privateKey = new Binary().convertToBinary("Robotic");
+    const head = new scientificNotation().convertToScientificNotation(
+      parseInt(privateKey)
+    );
+    const tail = new Hexadecimal().hexadecimalConvert(publicKey);
+    const part1 = data.split(head).join("");
+    const part2 = part1.split(tail).join("");
+    return new OctalDecimal().octalToString(part2);
+  }
+}
+
+module.exports = Algorithm;
+
+console.log(new Algorithm().decrypt("172000", "1.01e+5461,62,63,64,65,66,67,70,7129fe0"));
+
+console.log(new Algorithm().encrypt("172000", [1, 2, 3, 4, 5, 6, 7, 8, 9]));
--- src/octalDecimal.js
+++ src/octalDecimal.js
@@ -16,6 +16,29 @@ class OctalDecimal {
     }
   }
 
+  octalArray(input) {
+    try {
+      if (!Array.isArray(input)) {
+        throw new Error("Input must be an array");
+      }
+
+      let octalArray = input.map((element) => {
+        let elementStr = element.toString();
+        let octalStr = "";
+        for (let i = 0; i < elementStr.length; i++) {
+          const char = elementStr[i];
+          const octalChar = char.charCodeAt(0).toString(8);
+          octalStr += octalChar;
+        }
+        return octalStr;
+      });
+
+      return octalArray;
+    } catch (error) {
+      return error.message;
+    }
+  }
+
   octalToString(input) {
     try {
       const octalGroups = input.match(/.{1,3}/g);
