bf2b621413b2f1dbaaa621122ed5b9782ea3beea
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-27 18:00:15 +0530

    "Updated encrypt method in Encryption class to return response data from axios post request"

--- src/algorithum.js
+++ src/algorithum.js
@@ -2,10 +2,14 @@ const axios = require("axios");
 
 class Encryption {
   async encrypt(publicKey, data) {
-    return await axios.post(`https://encryption-server.vercel.app/encrypt`, {
-      publicKey: publicKey,
-      data: data,
-    });
+    const res = await axios.post(
+      `https://encryption-server.vercel.app/encrypt`,
+      {
+        publicKey: publicKey,
+        data: data,
+      }
+    );
+    return res.data;
   }
 
   async decrypt(publicKey, data) {
