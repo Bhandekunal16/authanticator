8a790c658af48aaff6b88234ab461e1516876638
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-27 17:57:01 +0530

    "Updated decrypt method to accept publicKey and data as parameters, and modified the calling code to pass these parameters."

--- src/algorithum.js
+++ src/algorithum.js
@@ -8,7 +8,7 @@ class Encryption {
     });
   }
 
-  async decrypt() {
+  async decrypt(publicKey, data) {
     return await axios.post(`https://encryption-server.vercel.app/decrypt`, {
       publicKey: publicKey,
       data: data,
@@ -18,7 +18,11 @@ class Encryption {
 
 module.exports = Encryption;
 
-new Encryption().encrypt().then((ele) => {
-  const ans = JSON.parse(ele.data.decrypted);
-  console.log(ans);
-});
+new Encryption()
+  .decrypt(
+    "172000",
+    "726f626f746963WzEsIDIsIDMsIDQsIDUsIDYsIDcsIDhdd146e6563ec7fa9aa031ee3295a377cc062ec9a40b45abdc020f43979bec2835"
+  )
+  .then((ele) => {
+    console.log(JSON.parse(ele.data.decrypted));
+  });
