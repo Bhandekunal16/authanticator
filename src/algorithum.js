43c912a8e5575bbf96a3ee98c8695f9fda6b6327
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-27 17:44:07 +0530

    "Refactor encryption functions into an Encryption class and export it"

--- src/algorithum.js
+++ src/algorithum.js
@@ -1,23 +1,19 @@
 const axios = require("axios");
 
-async function encrypt() {
-  return await axios.post(`https://encryption-server.vercel.app/encrypt`, {
-    publicKey: "Robotic.js",
-    data: "hello",
-  });
+class Encryption {
+  async encrypt() {
+    return await axios.post(`https://encryption-server.vercel.app/encrypt`, {
+      publicKey: "Robotic.js",
+      data: "hello",
+    });
+  }
+
+  async decrypt() {
+    return await axios.post(`https://encryption-server.vercel.app/decrypt`, {
+      publicKey: "Robotic.js",
+      data: "726f626f746963aGVsbG8=099f29be86cf77f32241bd81deb7038d37fbe4863bc66ab224f972133804ae0d",
+    });
+  }
 }
 
-async function decrypt() {
-  return await axios.post(`https://encryption-server.vercel.app/decrypt`, {
-    publicKey: "Robotic.js",
-    data: "726f626f746963aGVsbG8=099f29be86cf77f32241bd81deb7038d37fbe4863bc66ab224f972133804ae0d",
-  });
-}
-
-encrypt().then((ele) => {
-  console.log(ele.data);
-});
-
-decrypt().then((ele) => {
-  console.log(ele.data);
-});
+module.exports = Encryption;
