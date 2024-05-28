d816204f29ade86c9b2badb7c3908644bfc056f8
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-28 13:45:28 +0530

    update

---
+++ main.js
@@ -0,0 +1,21 @@
+const [scientificNotation, Binary, Hexadecimal, OctalDecimal, Encryption] = [
+  require("./src/scientificNotation"),
+  require("./src/binary"),
+  require("./src/hexadecimal"),
+  require("./src/octalDecimal"),
+  require("./src/algorithm"),
+];
+
+class Main {
+  constructor() {
+    this.scientificNotation = new scientificNotation();
+    this.binary = new Binary();
+    this.hexadecimal = new Hexadecimal();
+    this.octalDecimal = new OctalDecimal();
+    this.encryption = new Encryption();
+  }
+}
+
+exports["Main"] = Main;
+
+// ? console.log(new Main().binary.convertToBinary("kunal"));
