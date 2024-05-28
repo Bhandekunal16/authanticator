782c685add6a172696d4e51478db060cb8534d1f
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-28 10:06:35 +0530

    "Added ENCRYPTION module, removed src/algorithum.js and types/algorithum.d.ts files, and updated main.ts to include ENCRYPTION"

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
-const [Scientific_Notation, BINARY, HEXADECIMAL, OCTAL_DECIMAL] = [
+const [Scientific_Notation, BINARY, HEXADECIMAL, OCTAL_DECIMAL, ENCRYPTION] = [
   require("./src/scientificNotation"),
   require("./src/binary"),
   require("./src/hexadecimal"),
   require("./src/octalDecimal"),
+  require("./src/algorithm"),
 ];
 
 import scientificNotation from "./types/scientificNotation";
 import Binary from "./types/binary";
 import Hexadecimal from "./types/hexadecimal";
 import OctalDecimal from "./types/octalDecimal";
+import Encrypt from "./types/algorithm";
 
 export const ScientificNotation: scientificNotation = new Scientific_Notation();
 export const binary: Binary = new BINARY();
 export const hexadecimal: Hexadecimal = new HEXADECIMAL();
 export const Octal_decimal: OctalDecimal = new OCTAL_DECIMAL();
+export const Encryption: Encrypt = new ENCRYPTION();
