35622432fe3f9c6d55804629c67a2a08add70c09
bhandekunal16 <bhandekunal16@gmail.com>
2024-10-25 15:34:01 +0530

    "Extracted encryption and decryption API URLs to class properties"

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,25 +1,21 @@
 const axios = require("axios");
 
 class Encryption {
+  #Encrypt = `https://encryption-server.vercel.app/encrypt`;
+  #Decrypt = `https://encryption-server.vercel.app/decrypt`;
   async encrypt(publicKey, data) {
-    const res = await axios.post(
-      `https://encryption-server.vercel.app/encrypt`,
-      {
-        publicKey: publicKey,
-        data: data,
-      }
-    );
+    const res = await axios.post(this.#Encrypt, {
+      publicKey: publicKey,
+      data: data,
+    });
     return res.data;
   }
 
   async decrypt(publicKey, data) {
-    const res = await axios.post(
-      `https://encryption-server.vercel.app/decrypt`,
-      {
-        publicKey: publicKey,
-        data: data,
-      }
-    );
+    const res = await axios.post(this.#Decrypt, {
+      publicKey: publicKey,
+      data: data,
+    });
     return res.data;
   }
 }
